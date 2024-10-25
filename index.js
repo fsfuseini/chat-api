8e2c5a6995654920e75d5db608022d931ea5bd67
fsfuseini <suhuyini86@gmail.com>
2024-10-25 16:24:40 +0000

    more edits

--- index.js
+++ index.js
@@ -6,17 +6,56 @@ app.use(express.json());
 const PORT = 5000;
 
 const mockMessages = [
-  { id: 1, sender: "Rajesh", message: "Amy, good luck getting these guys excited about a dinner with a theme. I gave up when no one cared about my Tom Hanks-Giving." },
-  { id: 2, sender: "Wolowitz", message: "Hey, I threw out the first pitch at an Angels game." },
-  { id: 3, sender: "Sheldon", message: "The same thing that happened to Homo Erectus. He was replaced by a superior species." },
-  { id: 4, sender: "Leonard", message: "I've always been a little confused about this. Why don't Hindus eat beef?" },
-  { id: 5, sender: "Amy", message: "Where did you get empty Dove bar wrappers?" },
-  { id: 6, sender: "Bernadette", message: "Gosh, Amy. I'm sensing a little hostility. Is it maybe because like Sheldon's work, your sex life is also theoretical?" },
-  { id: 7, sender: "Penny", message: "What do you mean new roommate? What happened to Leonard?" },
+  {
+    id: 1,
+    sender: "Rajesh",
+    message: "Amy, good luck getting these guys excited about a dinner with a theme. I gave up when no one cared about my Tom Hanks-Giving."
+  },
+
+  {
+    id: 2,
+    sender: "Wolowitz",
+    message: "Hey, I threw out the first pitch at an Angels game."
+  },
+
+  {
+    id: 3,
+    sender: "Sheldon",
+    message: "The same thing that happened to Homo Erectus. He was replaced by a superior species."
+
+  },
+
+  {
+    id: 4,
+    sender: "Leonard",
+    message: "I've always been a little confused about this. Why don't Hindus eat beef?"
+
+  },
+
+  {
+    id: 5,
+    sender: "Amy",
+    message: "Where did you get empty Dove bar wrappers?"
+
+  },
+
+  {
+    id: 6,
+    sender: "Bernadette",
+    message: "Gosh, Amy. I'm sensing a little hostility. Is it maybe because like Sheldon's work, your sex life is also theoretical?"
+
+  },
+
+  {
+    id: 7,
+    sender: "Penny",
+    message: "What do you mean new roommate? What happened to Leonard?"
+
+  },
 ];
 
 app.listen(PORT, () => {
-  console.log(`Running on Port ${PORT}`);
+  console.log(`This Chat App is Running on Port ${PORT}`);
 });
 
 
@@ -33,7 +72,7 @@ app.get("/api/messages", (req, res) => {
 app.post("/api/messages", (req, res) => {
   console.log(req.body);
   const { body } = req;
-  const newMessage = {id: mockMessages[mockMessages.length - 1].id + 1, ...body};
+  const newMessage = { id: mockMessages[mockMessages.length - 1].id + 1, ...body };
   mockMessages.push(newMessage);
   return res.status(201).send(newMessage);
 });
@@ -43,19 +82,19 @@ app.get("/api/messages/:id", (req, res) => {
   console.log(req.params);
   const parsedId = parseInt(req.params.id);
   console.log(parsedId);
-  if (isNaN(parsedId)) return res.status(400).send({ msg: "Invalid ID." });
+  if (isNaN(parsedId)) return res.status(400).send({ msg: "Invalid message ID." });
   const findMessage = mockMessages.find((message) => message.id === parsedId);
-  if (!findMessage) return res.sendStatus(404);
-  return res.send(findMessage);
+  if (!findMessage) return res.status(404).send({ msg: "Message not found." });
+  return res.status(200).send(findMessage);
 });
 
 
 app.put("/api/messages/:id", (req, res) => {
   const { body, params: { id } } = req;
   const parsedId = parseInt(id);
-  if (isNaN(parsedId)) return res.status(400).send({ msg: "Invalid ID." });
+  if (isNaN(parsedId)) return res.status(400).send({ msg: "Invalid message ID." });
   const findMessageIndex = mockMessages.findIndex((message) => message.id === parsedId);
-  if (findMessageIndex === -1) return res.sendStatus(404);
+  if (findMessageIndex === -1) return res.status(404).send({ msg: "Message not found." });
   mockMessages[findMessageIndex] = { id: parsedId, ...body };
   return res.status(200).send(mockMessages[findMessageIndex]);
 });
@@ -63,7 +102,7 @@ app.put("/api/messages/:id", (req, res) => {
 app.patch("/api/messages/:id", (req, res) => {
   const { body, params: { id } } = req;
   const parsedId = parseInt(id);
-  if (isNaN(parsedId)) return res.status(400).send({ msg: "Invalid ID." });
+  if (isNaN(parsedId)) return res.status(400).send({ msg: "Invalid message ID." });
   const findMessageIndex = mockMessages.findIndex((message) => message.id === parsedId);
   if (findMessageIndex === -1) return res.sendStatus(404);
   mockMessages[findMessageIndex] = { ...mockMessages[findMessageIndex], ...body };
@@ -74,9 +113,9 @@ app.patch("/api/messages/:id", (req, res) => {
 app.delete("/api/messages/:id", (req, res) => {
   const { params: { id } } = req;
   const parsedId = parseInt(id);
-  if (isNaN(parsedId)) return res.status(400).send({ msg: "Invalid ID." });
+  if (isNaN(parsedId)) return res.status(400).send({ msg: "Invalid message ID." });
   const findMessageIndex = mockMessages.findIndex((message) => message.id === parsedId);
   if (findMessageIndex === -1) return res.sendStatus(404);
   mockMessages.splice(findMessageIndex, 1);
-  return res.send("Message deleted!");
+  return res.status(200).send("Message deleted!");
 });
